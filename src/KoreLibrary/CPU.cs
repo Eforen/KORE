02bc11066b5e4b6c9dbe81dd7007fb375f146b97
Ariel Lothlorien <git@arielkauai.com>
2020-07-05 12:23:16 -0500

    CPU Register Tests

--- src/KoreLibrary/CPU.cs
+++ src/KoreLibrary/CPU.cs
@@ -8,5 +8,118 @@ namespace Kore
 {
     public class CPU
     {
+        #region Register Handling
+
+        public enum Register: byte
+        {
+            /// <summary>
+            /// Accumulator
+            /// </summary>
+            A = 0,
+            /// <summary>
+            /// Base index (for use with arrays)
+            /// </summary>
+            B = 1,
+            /// <summary>
+            /// Counter (for use with loops, and strings, etc)
+            /// </summary>
+            C = 2,
+            /// <summary>
+            /// General Data Register
+            /// </summary>
+            D = 3,
+            /// <summary>
+            /// Stack pointer for top address of the stack
+            /// </summary>
+            SP = 4,
+            /// <summary>
+            /// Stack Base pointer for holding the address of the current stack frame
+            /// </summary>
+            BP = 5,
+            /// <summary>
+            /// Instruction pointer. Holds the program counter (the address of the next instruction).
+            /// </summary>
+            IP = 6
+        }
+
+        #region R?X Register
+        /// <summary>
+        /// Set Register R?X to ulong value
+        /// </summary>
+        /// <param name="register">Which R?X should be set</param>W
+        /// <param name="value">What value to place in register. Converted to non signed ulong Irelivent of the type it actually is.</param>
+        public void setRX(Register register, ulong value) { }
+
+        /// <summary>
+        /// Get Register R?X to ulong value
+        /// </summary>
+        /// <param name="register">Which R?X should be returned</param>
+        /// <returns>The Register Value as a ulong no mater what the actual value is.</returns>
+        public ulong getRX(Register register) { return 0; }
+        #endregion // End of R?X Register
+
+        #region E?X Register
+        /// <summary>
+        /// Set Register E?X to uint value
+        /// </summary>
+        /// <param name="register">Which E?X should be set</param>
+        /// <param name="value">What value to place in register. Converted to non signed uint Irelivent of the type it actually is.</param>
+        public void setEX(Register register, uint value) { }
+
+        /// <summary>
+        /// Get Register E?X to uint value
+        /// </summary>
+        /// <param name="register">Which E?X should be returned</param>
+        /// <returns>The Register Value as a uint no mater what the actual value is.</returns>
+        public uint getEX(Register register) { return 0; }
+        #endregion // End of E?X Register
+
+        #region ?X Register
+        /// <summary>
+        /// Set Register ?X to ushort value
+        /// </summary>
+        /// <param name="register">Which ?X should be set</param>
+        /// <param name="value">What value to place in register. Converted to non signed ushort Irelivent of the type it actually is.</param>
+        public void setX(Register register, ushort value) { }
+
+        /// <summary>
+        /// Get Register ?X to ushort value
+        /// </summary>
+        /// <param name="register">Which ?X should be returned</param>
+        /// <returns>The Register Value as a ushort no mater what the actual value is.</returns>
+        public ushort getX(Register register) { return 0; }
+        #endregion // End of ?X Register
+
+        #region ?H Register
+        /// <summary>
+        /// Set Register ?H to byte value
+        /// </summary>
+        /// <param name="register">Which ?H should be set</param>
+        /// <param name="value">What value to place in register. Converted to non signed byte Irelivent of the type it actually is.</param>
+        public void setH(Register register, byte value) { }
+
+        /// <summary>
+        /// Get Register ?H to byte value
+        /// </summary>
+        /// <param name="register">Which ?H should be returned</param>
+        /// <returns>The Register Value as a byte no mater what the actual value is.</returns>
+        public byte getH(Register register) { return 0; }
+        #endregion // End of ?H Register
+        #region ?L Register
+        /// <summary>
+        /// Set Register ?L to byte value
+        /// </summary>
+        /// <param name="register">Which ?L should be set</param>
+        /// <param name="value">What value to place in register. Converted to non signed byte Irelivent of the type it actually is.</param>
+        public void setL(Register register, byte value) { }
+
+        /// <summary>
+        /// Get Register ?L to byte value
+        /// </summary>
+        /// <param name="register">Which ?L should be returned</param>
+        /// <returns>The Register Value as a byte no mater what the actual value is.</returns>
+        public byte getL(Register register) { return 0; }
+        #endregion // End of ?L Register
+        #endregion // End of Register Handling
     }
 }
--- src/KoreTests/CPUTests.cs
+++ src/KoreTests/CPUTests.cs
@@ -4,16 +4,302 @@ namespace KoreTests
 {
     public class CPUTests
     {
+        private Kore.CPU cpu;
         [SetUp]
         public void Setup()
         {
+            cpu = new Kore.CPU();
         }
 
         [Test]
         public void Instantiation()
         {
-            Kore.CPU cpu = new Kore.CPU();
             Assert.IsInstanceOf<Kore.CPU>(cpu);
+        }
+
+        [TestCase(Kore.CPU.Register.A, 0, 0x29928a0bac818116, 0x952d2a4c42c3b899)]
+        [TestCase(Kore.CPU.Register.A, 0, 0xec8525bd287984a6, 0x374814b7cc672d12)]
+        [TestCase(Kore.CPU.Register.A, 0, 0x1811dc85da0d217e, 0xff43757f49b222f1)]
+        [TestCase(Kore.CPU.Register.A, 0, 0x842843ad1907db58, 0xba9d3e0c4cd68b66)]
+        [TestCase(Kore.CPU.Register.A, 0, 0xea2b62db634c485f, 0xf03ed33799359b5d)]
+        [TestCase(Kore.CPU.Register.A, 0, 0x204f36cfdc6aff52, 0x742c59eb74b11a06)]
+
+        [TestCase(Kore.CPU.Register.B, 0, 0x29928a0bac818116, 0x952d2a4c42c3b899)]
+        [TestCase(Kore.CPU.Register.B, 0, 0xec8525bd287984a6, 0x374814b7cc672d12)]
+        [TestCase(Kore.CPU.Register.B, 0, 0x1811dc85da0d217e, 0xff43757f49b222f1)]
+        [TestCase(Kore.CPU.Register.B, 0, 0x842843ad1907db58, 0xba9d3e0c4cd68b66)]
+        [TestCase(Kore.CPU.Register.B, 0, 0xea2b62db634c485f, 0xf03ed33799359b5d)]
+        [TestCase(Kore.CPU.Register.B, 0, 0x204f36cfdc6aff52, 0x742c59eb74b11a06)]
+
+        [TestCase(Kore.CPU.Register.BP, 0, 0x29928a0bac818116, 0x952d2a4c42c3b899)]
+        [TestCase(Kore.CPU.Register.BP, 0, 0xec8525bd287984a6, 0x374814b7cc672d12)]
+        [TestCase(Kore.CPU.Register.BP, 0, 0x1811dc85da0d217e, 0xff43757f49b222f1)]
+        [TestCase(Kore.CPU.Register.BP, 0, 0x842843ad1907db58, 0xba9d3e0c4cd68b66)]
+        [TestCase(Kore.CPU.Register.BP, 0, 0xea2b62db634c485f, 0xf03ed33799359b5d)]
+        [TestCase(Kore.CPU.Register.BP, 0, 0x204f36cfdc6aff52, 0x742c59eb74b11a06)]
+
+        [TestCase(Kore.CPU.Register.C, 0, 0x29928a0bac818116, 0x952d2a4c42c3b899)]
+        [TestCase(Kore.CPU.Register.C, 0, 0xec8525bd287984a6, 0x374814b7cc672d12)]
+        [TestCase(Kore.CPU.Register.C, 0, 0x1811dc85da0d217e, 0xff43757f49b222f1)]
+        [TestCase(Kore.CPU.Register.C, 0, 0x842843ad1907db58, 0xba9d3e0c4cd68b66)]
+        [TestCase(Kore.CPU.Register.C, 0, 0xea2b62db634c485f, 0xf03ed33799359b5d)]
+        [TestCase(Kore.CPU.Register.C, 0, 0x204f36cfdc6aff52, 0x742c59eb74b11a06)]
+
+        [TestCase(Kore.CPU.Register.D, 0, 0x29928a0bac818116, 0x952d2a4c42c3b899)]
+        [TestCase(Kore.CPU.Register.D, 0, 0xec8525bd287984a6, 0x374814b7cc672d12)]
+        [TestCase(Kore.CPU.Register.D, 0, 0x1811dc85da0d217e, 0xff43757f49b222f1)]
+        [TestCase(Kore.CPU.Register.D, 0, 0x842843ad1907db58, 0xba9d3e0c4cd68b66)]
+        [TestCase(Kore.CPU.Register.D, 0, 0xea2b62db634c485f, 0xf03ed33799359b5d)]
+        [TestCase(Kore.CPU.Register.D, 0, 0x204f36cfdc6aff52, 0x742c59eb74b11a06)]
+
+        [TestCase(Kore.CPU.Register.IP, 0, 0x29928a0bac818116, 0x952d2a4c42c3b899)]
+        [TestCase(Kore.CPU.Register.IP, 0, 0xec8525bd287984a6, 0x374814b7cc672d12)]
+        [TestCase(Kore.CPU.Register.IP, 0, 0x1811dc85da0d217e, 0xff43757f49b222f1)]
+        [TestCase(Kore.CPU.Register.IP, 0, 0x842843ad1907db58, 0xba9d3e0c4cd68b66)]
+        [TestCase(Kore.CPU.Register.IP, 0, 0xea2b62db634c485f, 0xf03ed33799359b5d)]
+        [TestCase(Kore.CPU.Register.IP, 0, 0x204f36cfdc6aff52, 0x742c59eb74b11a06)]
+
+        [TestCase(Kore.CPU.Register.SP, 0, 0x29928a0bac818116, 0x952d2a4c42c3b899)]
+        [TestCase(Kore.CPU.Register.SP, 0, 0xec8525bd287984a6, 0x374814b7cc672d12)]
+        [TestCase(Kore.CPU.Register.SP, 0, 0x1811dc85da0d217e, 0xff43757f49b222f1)]
+        [TestCase(Kore.CPU.Register.SP, 0, 0x842843ad1907db58, 0xba9d3e0c4cd68b66)]
+        [TestCase(Kore.CPU.Register.SP, 0, 0xea2b62db634c485f, 0xf03ed33799359b5d)]
+        [TestCase(Kore.CPU.Register.SP, 0, 0x204f36cfdc6aff52, 0x742c59eb74b11a06)]
+        public void TestRegister08Bytes(Kore.CPU.Register r, ulong startExpectation, ulong target1, ulong target2)
+        {
+            Assert.AreEqual(startExpectation, cpu.getRX(r));
+            cpu.setRX(r, target1);
+            Assert.AreEqual(target1, cpu.getRX(r));
+            cpu.setRX(r, target2);
+            Assert.AreEqual(target2, cpu.getRX(r));
+        }
+
+        [TestCase(Kore.CPU.Register.A, 0, 0x29928a0b, 0x952d2a4c)]
+        [TestCase(Kore.CPU.Register.A, 0, 0xec8525bd, 0x374814b7)]
+        [TestCase(Kore.CPU.Register.A, 0, 0x1811dc85, 0xff43757f)]
+        [TestCase(Kore.CPU.Register.A, 0, 0x842843ad, 0xba9d3e0c)]
+        [TestCase(Kore.CPU.Register.A, 0, 0xea2b62db, 0xf03ed337)]
+        [TestCase(Kore.CPU.Register.A, 0, 0x204f36cf, 0x742c59eb)]
+
+        [TestCase(Kore.CPU.Register.B, 0, 0x29928a0b, 0x952d2a4c)]
+        [TestCase(Kore.CPU.Register.B, 0, 0xec8525bd, 0x374814b7)]
+        [TestCase(Kore.CPU.Register.B, 0, 0x1811dc85, 0xff43757f)]
+        [TestCase(Kore.CPU.Register.B, 0, 0x842843ad, 0xba9d3e0c)]
+        [TestCase(Kore.CPU.Register.B, 0, 0xea2b62db, 0xf03ed337)]
+        [TestCase(Kore.CPU.Register.B, 0, 0x204f36cf, 0x742c59eb)]
+
+        [TestCase(Kore.CPU.Register.BP, 0, 0x29928a0b, 0x952d2a4c)]
+        [TestCase(Kore.CPU.Register.BP, 0, 0xec8525bd, 0x374814b7)]
+        [TestCase(Kore.CPU.Register.BP, 0, 0x1811dc85, 0xff43757f)]
+        [TestCase(Kore.CPU.Register.BP, 0, 0x842843ad, 0xba9d3e0c)]
+        [TestCase(Kore.CPU.Register.BP, 0, 0xea2b62db, 0xf03ed337)]
+        [TestCase(Kore.CPU.Register.BP, 0, 0x204f36cf, 0x742c59eb)]
+
+        [TestCase(Kore.CPU.Register.C, 0, 0x29928a0b, 0x952d2a4c)]
+        [TestCase(Kore.CPU.Register.C, 0, 0xec8525bd, 0x374814b7)]
+        [TestCase(Kore.CPU.Register.C, 0, 0x1811dc85, 0xff43757f)]
+        [TestCase(Kore.CPU.Register.C, 0, 0x842843ad, 0xba9d3e0c)]
+        [TestCase(Kore.CPU.Register.C, 0, 0xea2b62db, 0xf03ed337)]
+        [TestCase(Kore.CPU.Register.C, 0, 0x204f36cf, 0x742c59eb)]
+
+        [TestCase(Kore.CPU.Register.D, 0, 0x29928a0b, 0x952d2a4c)]
+        [TestCase(Kore.CPU.Register.D, 0, 0xec8525bd, 0x374814b7)]
+        [TestCase(Kore.CPU.Register.D, 0, 0x1811dc85, 0xff43757f)]
+        [TestCase(Kore.CPU.Register.D, 0, 0x842843ad, 0xba9d3e0c)]
+        [TestCase(Kore.CPU.Register.D, 0, 0xea2b62db, 0xf03ed337)]
+        [TestCase(Kore.CPU.Register.D, 0, 0x204f36cf, 0x742c59eb)]
+
+        [TestCase(Kore.CPU.Register.IP, 0, 0x29928a0b, 0x952d2a4c)]
+        [TestCase(Kore.CPU.Register.IP, 0, 0xec8525bd, 0x374814b7)]
+        [TestCase(Kore.CPU.Register.IP, 0, 0x1811dc85, 0xff43757f)]
+        [TestCase(Kore.CPU.Register.IP, 0, 0x842843ad, 0xba9d3e0c)]
+        [TestCase(Kore.CPU.Register.IP, 0, 0xea2b62db, 0xf03ed337)]
+        [TestCase(Kore.CPU.Register.IP, 0, 0x204f36cf, 0x742c59eb)]
+
+        [TestCase(Kore.CPU.Register.SP, 0, 0x29928a0b, 0x952d2a4c)]
+        [TestCase(Kore.CPU.Register.SP, 0, 0xec8525bd, 0x374814b7)]
+        [TestCase(Kore.CPU.Register.SP, 0, 0x1811dc85, 0xff43757f)]
+        [TestCase(Kore.CPU.Register.SP, 0, 0x842843ad, 0xba9d3e0c)]
+        [TestCase(Kore.CPU.Register.SP, 0, 0xea2b62db, 0xf03ed337)]
+        [TestCase(Kore.CPU.Register.SP, 0, 0x204f36cf, 0x742c59eb)]
+        public void TestRegister04Bytes(Kore.CPU.Register r, uint startExpectation, uint target1, uint target2)
+        {
+            Assert.AreEqual(startExpectation, cpu.getEX(r));
+            cpu.setEX(r, target1);
+            Assert.AreEqual(target1, cpu.getEX(r));
+            cpu.setEX(r, target2);
+            Assert.AreEqual(target2, cpu.getEX(r));
+        }
+
+        [TestCase(Kore.CPU.Register.A, 0, 0x2992, 0x952d)]
+        [TestCase(Kore.CPU.Register.A, 0, 0xec85, 0x3748)]
+        [TestCase(Kore.CPU.Register.A, 0, 0x1811, 0xff43)]
+        [TestCase(Kore.CPU.Register.A, 0, 0x8428, 0xba9d)]
+        [TestCase(Kore.CPU.Register.A, 0, 0xea2b, 0xf03e)]
+        [TestCase(Kore.CPU.Register.A, 0, 0x204f, 0x742c)]
+
+        [TestCase(Kore.CPU.Register.B, 0, 0x2992, 0x952d)]
+        [TestCase(Kore.CPU.Register.B, 0, 0xec85, 0x3748)]
+        [TestCase(Kore.CPU.Register.B, 0, 0x1811, 0xff43)]
+        [TestCase(Kore.CPU.Register.B, 0, 0x8428, 0xba9d)]
+        [TestCase(Kore.CPU.Register.B, 0, 0xea2b, 0xf03e)]
+        [TestCase(Kore.CPU.Register.B, 0, 0x204f, 0x742c)]
+
+        [TestCase(Kore.CPU.Register.BP, 0, 0x2992, 0x952d)]
+        [TestCase(Kore.CPU.Register.BP, 0, 0xec85, 0x3748)]
+        [TestCase(Kore.CPU.Register.BP, 0, 0x1811, 0xff43)]
+        [TestCase(Kore.CPU.Register.BP, 0, 0x8428, 0xba9d)]
+        [TestCase(Kore.CPU.Register.BP, 0, 0xea2b, 0xf03e)]
+        [TestCase(Kore.CPU.Register.BP, 0, 0x204f, 0x742c)]
+
+        [TestCase(Kore.CPU.Register.C, 0, 0x2992, 0x952d)]
+        [TestCase(Kore.CPU.Register.C, 0, 0xec85, 0x3748)]
+        [TestCase(Kore.CPU.Register.C, 0, 0x1811, 0xff43)]
+        [TestCase(Kore.CPU.Register.C, 0, 0x8428, 0xba9d)]
+        [TestCase(Kore.CPU.Register.C, 0, 0xea2b, 0xf03e)]
+        [TestCase(Kore.CPU.Register.C, 0, 0x204f, 0x742c)]
+
+        [TestCase(Kore.CPU.Register.D, 0, 0x2992, 0x952d)]
+        [TestCase(Kore.CPU.Register.D, 0, 0xec85, 0x3748)]
+        [TestCase(Kore.CPU.Register.D, 0, 0x1811, 0xff43)]
+        [TestCase(Kore.CPU.Register.D, 0, 0x8428, 0xba9d)]
+        [TestCase(Kore.CPU.Register.D, 0, 0xea2b, 0xf03e)]
+        [TestCase(Kore.CPU.Register.D, 0, 0x204f, 0x742c)]
+
+        [TestCase(Kore.CPU.Register.IP, 0, 0x2992, 0x952d)]
+        [TestCase(Kore.CPU.Register.IP, 0, 0xec85, 0x3748)]
+        [TestCase(Kore.CPU.Register.IP, 0, 0x1811, 0xff43)]
+        [TestCase(Kore.CPU.Register.IP, 0, 0x8428, 0xba9d)]
+        [TestCase(Kore.CPU.Register.IP, 0, 0xea2b, 0xf03e)]
+        [TestCase(Kore.CPU.Register.IP, 0, 0x204f, 0x742c)]
+
+        [TestCase(Kore.CPU.Register.SP, 0, 0x2992, 0x952d)]
+        [TestCase(Kore.CPU.Register.SP, 0, 0xec85, 0x3748)]
+        [TestCase(Kore.CPU.Register.SP, 0, 0x1811, 0xff43)]
+        [TestCase(Kore.CPU.Register.SP, 0, 0x8428, 0xba9d)]
+        [TestCase(Kore.CPU.Register.SP, 0, 0xea2b, 0xf03e)]
+        [TestCase(Kore.CPU.Register.SP, 0, 0x204f, 0x742c)]
+        public void TestRegister02Bytes(Kore.CPU.Register r, ushort startExpectation, ushort target1, ushort target2)
+        {
+            Assert.AreEqual(startExpectation, cpu.getX(r));
+            cpu.setX(r, target1);
+            Assert.AreEqual(target1, cpu.getX(r));
+            cpu.setX(r, target2);
+            Assert.AreEqual(target2, cpu.getX(r));
+        }
+
+        [TestCase(Kore.CPU.Register.A, 0, 0x29, 0x95)]
+        [TestCase(Kore.CPU.Register.A, 0, 0xec, 0x37)]
+        [TestCase(Kore.CPU.Register.A, 0, 0x18, 0xff)]
+        [TestCase(Kore.CPU.Register.A, 0, 0x84, 0xba)]
+        [TestCase(Kore.CPU.Register.A, 0, 0xea, 0xf0)]
+        [TestCase(Kore.CPU.Register.A, 0, 0x20, 0x74)]
+
+        [TestCase(Kore.CPU.Register.B, 0, 0x29, 0x95)]
+        [TestCase(Kore.CPU.Register.B, 0, 0xec, 0x37)]
+        [TestCase(Kore.CPU.Register.B, 0, 0x18, 0xff)]
+        [TestCase(Kore.CPU.Register.B, 0, 0x84, 0xba)]
+        [TestCase(Kore.CPU.Register.B, 0, 0xea, 0xf0)]
+        [TestCase(Kore.CPU.Register.B, 0, 0x20, 0x74)]
+
+        [TestCase(Kore.CPU.Register.BP, 0, 0x29, 0x95)]
+        [TestCase(Kore.CPU.Register.BP, 0, 0xec, 0x37)]
+        [TestCase(Kore.CPU.Register.BP, 0, 0x18, 0xff)]
+        [TestCase(Kore.CPU.Register.BP, 0, 0x84, 0xba)]
+        [TestCase(Kore.CPU.Register.BP, 0, 0xea, 0xf0)]
+        [TestCase(Kore.CPU.Register.BP, 0, 0x20, 0x74)]
+
+        [TestCase(Kore.CPU.Register.C, 0, 0x29, 0x95)]
+        [TestCase(Kore.CPU.Register.C, 0, 0xec, 0x37)]
+        [TestCase(Kore.CPU.Register.C, 0, 0x18, 0xff)]
+        [TestCase(Kore.CPU.Register.C, 0, 0x84, 0xba)]
+        [TestCase(Kore.CPU.Register.C, 0, 0xea, 0xf0)]
+        [TestCase(Kore.CPU.Register.C, 0, 0x20, 0x74)]
+
+        [TestCase(Kore.CPU.Register.D, 0, 0x29, 0x95)]
+        [TestCase(Kore.CPU.Register.D, 0, 0xec, 0x37)]
+        [TestCase(Kore.CPU.Register.D, 0, 0x18, 0xff)]
+        [TestCase(Kore.CPU.Register.D, 0, 0x84, 0xba)]
+        [TestCase(Kore.CPU.Register.D, 0, 0xea, 0xf0)]
+        [TestCase(Kore.CPU.Register.D, 0, 0x20, 0x74)]
+
+        [TestCase(Kore.CPU.Register.IP, 0, 0x29, 0x95)]
+        [TestCase(Kore.CPU.Register.IP, 0, 0xec, 0x37)]
+        [TestCase(Kore.CPU.Register.IP, 0, 0x18, 0xff)]
+        [TestCase(Kore.CPU.Register.IP, 0, 0x84, 0xba)]
+        [TestCase(Kore.CPU.Register.IP, 0, 0xea, 0xf0)]
+        [TestCase(Kore.CPU.Register.IP, 0, 0x20, 0x74)]
+
+        [TestCase(Kore.CPU.Register.SP, 0, 0x29, 0x95)]
+        [TestCase(Kore.CPU.Register.SP, 0, 0xec, 0x37)]
+        [TestCase(Kore.CPU.Register.SP, 0, 0x18, 0xff)]
+        [TestCase(Kore.CPU.Register.SP, 0, 0x84, 0xba)]
+        [TestCase(Kore.CPU.Register.SP, 0, 0xea, 0xf0)]
+        [TestCase(Kore.CPU.Register.SP, 0, 0x20, 0x74)]
+        public void TestRegisterHighByte(Kore.CPU.Register r, byte startExpectation, byte target1, byte target2)
+        {
+            Assert.AreEqual(startExpectation, cpu.getH(r));
+            cpu.setH(r, target1);
+            Assert.AreEqual(target1, cpu.getH(r));
+            cpu.setH(r, target2);
+            Assert.AreEqual(target2, cpu.getH(r));
+        }
+
+        [TestCase(Kore.CPU.Register.A, 0, 0x92, 0x2d)]
+        [TestCase(Kore.CPU.Register.A, 0, 0x85, 0x48)]
+        [TestCase(Kore.CPU.Register.A, 0, 0x11, 0x43)]
+        [TestCase(Kore.CPU.Register.A, 0, 0x28, 0x9d)]
+        [TestCase(Kore.CPU.Register.A, 0, 0x2b, 0x3e)]
+        [TestCase(Kore.CPU.Register.A, 0, 0x4f, 0x2c)]
+
+        [TestCase(Kore.CPU.Register.B, 0, 0x92, 0x2d)]
+        [TestCase(Kore.CPU.Register.B, 0, 0x85, 0x48)]
+        [TestCase(Kore.CPU.Register.B, 0, 0x11, 0x43)]
+        [TestCase(Kore.CPU.Register.B, 0, 0x28, 0x9d)]
+        [TestCase(Kore.CPU.Register.B, 0, 0x2b, 0x3e)]
+        [TestCase(Kore.CPU.Register.B, 0, 0x4f, 0x2c)]
+
+        [TestCase(Kore.CPU.Register.BP, 0, 0x92, 0x2d)]
+        [TestCase(Kore.CPU.Register.BP, 0, 0x85, 0x48)]
+        [TestCase(Kore.CPU.Register.BP, 0, 0x11, 0x43)]
+        [TestCase(Kore.CPU.Register.BP, 0, 0x28, 0x9d)]
+        [TestCase(Kore.CPU.Register.BP, 0, 0x2b, 0x3e)]
+        [TestCase(Kore.CPU.Register.BP, 0, 0x4f, 0x2c)]
+
+        [TestCase(Kore.CPU.Register.C, 0, 0x92, 0x2d)]
+        [TestCase(Kore.CPU.Register.C, 0, 0x85, 0x48)]
+        [TestCase(Kore.CPU.Register.C, 0, 0x11, 0x43)]
+        [TestCase(Kore.CPU.Register.C, 0, 0x28, 0x9d)]
+        [TestCase(Kore.CPU.Register.C, 0, 0x2b, 0x3e)]
+        [TestCase(Kore.CPU.Register.C, 0, 0x4f, 0x2c)]
+
+        [TestCase(Kore.CPU.Register.D, 0, 0x92, 0x2d)]
+        [TestCase(Kore.CPU.Register.D, 0, 0x85, 0x48)]
+        [TestCase(Kore.CPU.Register.D, 0, 0x11, 0x43)]
+        [TestCase(Kore.CPU.Register.D, 0, 0x28, 0x9d)]
+        [TestCase(Kore.CPU.Register.D, 0, 0x2b, 0x3e)]
+        [TestCase(Kore.CPU.Register.D, 0, 0x4f, 0x2c)]
+
+        [TestCase(Kore.CPU.Register.IP, 0, 0x92, 0x2d)]
+        [TestCase(Kore.CPU.Register.IP, 0, 0x85, 0x48)]
+        [TestCase(Kore.CPU.Register.IP, 0, 0x11, 0x43)]
+        [TestCase(Kore.CPU.Register.IP, 0, 0x28, 0x9d)]
+        [TestCase(Kore.CPU.Register.IP, 0, 0x2b, 0x3e)]
+        [TestCase(Kore.CPU.Register.IP, 0, 0x4f, 0x2c)]
+
+        [TestCase(Kore.CPU.Register.SP, 0, 0x92, 0x2d)]
+        [TestCase(Kore.CPU.Register.SP, 0, 0x85, 0x48)]
+        [TestCase(Kore.CPU.Register.SP, 0, 0x11, 0x43)]
+        [TestCase(Kore.CPU.Register.SP, 0, 0x28, 0x9d)]
+        [TestCase(Kore.CPU.Register.SP, 0, 0x2b, 0x3e)]
+        [TestCase(Kore.CPU.Register.SP, 0, 0x4f, 0x2c)]
+        public void TestRegisterLowByte(Kore.CPU.Register r, byte startExpectation, byte target1, byte target2)
+        {
+            Assert.AreEqual(startExpectation, cpu.getL(r));
+            cpu.setL(r, target1);
+            Assert.AreEqual(target1, cpu.getL(r));
+            cpu.setL(r, target2);
+            Assert.AreEqual(target2, cpu.getL(r));
         }
     }
 }
